3c5c11e33af1f70533424d6ee581c2362fd6ed47
Naima <naima.zida8@gmail.com>
2026-05-16 22:23:48 -0700

    SortBy component has search preferences passed in so other components can access. species search result component added.

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import SortBy from "../components/SortBy/SortBy";
+import SpeciesSearchResult from "../components/SpeciesSearchResult/SpeciesSearchResult";
 
 function SearchPage() {
-const options = {
-    "Most Liked": "most_liked",
-    "Most Sprayed": "most_sprayed",
-    "Posted Today": "posted_today"
-};
+    const options = {
+        "Most Liked": "most_liked",
+        "Most Sprayed": "most_sprayed",
+        "Posted Today": "posted_today"
+    };
+    const [sortBy, setSortBy] = useState("");
+    const [speciesQuery, setSpeciesQuery] = useState("");
 
     return (
         <>
-        <SortBy options={options} />
+            <SortBy options={options} sortBy={sortBy} setSortBy={setSortBy} speciesQuery={speciesQuery} setSpeciesQuery={setSpeciesQuery} />
+            <SpeciesSearchResult species={speciesQuery} />
         </>
     )
 }
