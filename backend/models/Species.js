d42782d889116163798ae4711e4687e8c5fe846a
ijh22 <ijh22@icloud.com>
2026-05-19 00:28:10 -0700

    Defined the species schema in Species.js

--- backend/models/Species.js
+++ backend/models/Species.js
@@ -1,13 +1,29 @@
-// TODO: Convert the species json into a schema
 import mongoose from "mongoose";
 
+//Define the schema for the species collection
 const speciesSchema = new mongoose.Schema(
-  {},
+  {
+    speciesCommon: String,
+    speciesActual: String,
+    Kingdom: String,
+    Phylum: String,
+    Class: String,
+    Order: String,
+    Family: String,
+    Genus: String,
+    Species: String,
+    Size: String,
+    Colors: String,
+    Descriptors: String,
+    Category: String,
+    imageUrl: String,
+  },
   {
     timestamps: true,
   },
 );
 
+//Convert the species json into a schema
 const Species = mongoose.model("Species", speciesSchema);
 
 export default Species;
